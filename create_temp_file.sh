5931f6d8aac374a870a3cfc9993f33d56f3584dd
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2016-03-11 09:48:38 -0300

    master: refactor

--- create_temp_file.sh
+++ create_temp_file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATE=`date +"%Y/%m/%d %H:%M:%S"`;
-echo "PROCESS COPY[$PROCESS_NUMBER] Writing 'SUPERVISOR_PROCESS_VAR' [$SUPERVISOR_PROCESS_VAR] at $DATE" >> /tmp/test-supervidor.txt &
+echo "PROCESS COPY[$PROCESS_NUMBER] Writing 'SUPERVISOR_PROCESS_VAR' [$SUPERVISOR_PROCESS_VAR] at $DATE" >> /tmp/test-supervidor.txt
